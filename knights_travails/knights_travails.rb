82a611c89211b3ea62843e8ce45782a2a42f9bc0
Brillante Wang <brillante.wang@gmail.com>
2017-10-07 18:32:23 -0700

    Define #find_path

--- knights_travails/knights_travails.rb
+++ knights_travails/knights_travails.rb
@@ -1,48 +1,78 @@
 require '../polytreenode/lib/00_tree_node.rb'
 
 class KnightPathFinder
+  MOVES = [
+    [-2, 1],
+    [-1, 2],
+    [1, 2],
+    [2, 1],
+    [2, -1],
+    [1, -2],
+    [-1, -2],
+    [-2, -1]
+  ]
+
   def initialize(start_pos)
     @start_pos = start_pos
     @visited_positions = [start_pos]
-    @grid = Array.new(8) { Array.new(8) }
 
     build_move_tree
   end
 
-  def populate_nodes
-    @grid.each_with_index do |row, row_idx|
-      row.each_index do |col_idx|
-        pos = [row_idx, col_idx]
-        self[pos] = PolyTreeNode.new(pos)
+  def find_path(end_pos)
+    end_node = @root_node.dfs(end_pos)
+
+    trace_path_back(end_node).reverse.map(&:value)
+  end
+
+  def trace_path_back(end_node)
+    path = []
+
+    curr_node = end_node
+    until curr_node.nil?
+      path << curr_node
+      curr_node = curr_node.parent
+    end
+
+    path
+  end
+
+  def build_move_tree
+    @root_node = PolyTreeNode.new(@start_pos)
+    queue = [@root_node]
+
+    until queue.empty?
+      curr_node = queue.shift
+      curr_pos = curr_node.value
+
+      new_move_positions(curr_pos).each do |new_pos|
+        new_node = PolyTreeNode.new(new_pos)
+        curr_node.add_child(new_node)
+        queue << new_node
       end
     end
   end
 
-  def build_move_tree
-    queue = []
-
-
-  end
-
   def self.valid_moves(pos)
+    # debugger
+    moves = []
+    curr_x, curr_y = pos
+
+    MOVES.each do |(shift_x, shift_y)|
+      new_pos = [curr_x + shift_x, curr_y + shift_y]
+      moves << new_pos if in_range?(new_pos)
+    end
+
+    moves
   end
 
   def new_move_positions(pos)
-    self.class.valid_moves - @visited_positions
+    new_moves = self.class.valid_moves(pos) - @visited_positions
+    @visited_positions += new_moves
+    new_moves
   end
 
-  def [](pos)
-    row, col = pos
-    @grid[row][col]
+  def self.in_range?(pos)
+    pos.all? { |n| n.between?(0, 7) }
   end
-
-  def []=(pos, val)
-    row, col = pos
-    @grid[row][col] = val
-  end
-
-  def in_range?(pos)
-    pos.all? { |n| n.between?(0, @grid.size - 1) }
-  end
-
 end
